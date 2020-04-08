5ca55b71786e9af6c206f97dae67ecda25347982
박태원 <producer82@naver.com>
2020-04-09 00:31:36 +0900

    Activity 1

--- AndroidLayout/app/src/main/java/com/example/androidlayout/MainActivity.java
+++ AndroidLayout/app/src/main/java/com/example/androidlayout/MainActivity.java
@@ -2,13 +2,41 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+ public class MainActivity extends AppCompatActivity {
+    TextInputEditText TextInputEditText_Email;
+    TextInputEditText TextInputEditText_Password;
+    Button Button_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_Email = findViewById(R.id.TextInputTextEdit_Email);
+        TextInputEditText_Password = findViewById(R.id.TextInputTextEdit_Password);
+        Button_Login = findViewById(R.id.Button_Login);
+
+        Button_Login.setClickable(true);
+        Button_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = TextInputEditText_Email.getText().toString();
+                String password = TextInputEditText_Password.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+
+                startActivity(intent);
+            }
+        });
     }
 }
