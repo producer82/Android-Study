c5d1d4dfa2fce56f0a5daa695d83b05a002614e4
박태원 <producer82@naver.com>
2020-04-10 23:43:35 +0900

    Activity 2

--- AndroidLayout/app/src/main/java/com/example/androidlayout/MainActivity.java
+++ AndroidLayout/app/src/main/java/com/example/androidlayout/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +17,10 @@ import com.google.android.material.textfield.TextInputEditText;
     TextInputEditText TextInputEditText_Email;
     TextInputEditText TextInputEditText_Password;
     Button Button_Login;
+    String emailOK = "123@gmail.com";
+    String passwordOK = "1234";
+    String inputEmail = "";
+    String inputPassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,49 @@ import com.google.android.material.textfield.TextInputEditText;
         TextInputEditText_Password = findViewById(R.id.TextInputTextEdit_Password);
         Button_Login = findViewById(R.id.Button_Login);
 
-        Button_Login.setClickable(true);
+        Button_Login.setClickable(false);
+
+        TextInputEditText_Email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s != null) {
+                    inputEmail = s.toString();
+                    Button_Login.setClickable(validation());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        TextInputEditText_Password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s != null){
+                    inputPassword = s.toString();
+                    Button_Login.setClickable(validation());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Button_Login.setClickable(true);
         Button_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,5 +87,9 @@ import com.google.android.material.textfield.TextInputEditText;
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean validation() {
+        return inputEmail.equals(emailOK) && inputPassword.equals(passwordOK);
     }
 }
